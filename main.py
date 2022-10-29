2163c9161cd0851e7f5417c976791ae9bf5d907a
Vasyl Pidhirskyi <sdilmob@gmail.com>
2022-10-29 12:58:28 +0300

    2. Output an Image

---
+++ main.py
@@ -0,0 +1,18 @@
+import taichi as ti
+
+
+ti.init(arch=ti.gpu)
+
+width, height = 256, 256
+pixels = ti.Vector.field(n=3, dtype=ti.f32, shape=(width, height))
+
+@ti.kernel
+def paint():
+    for i, j in pixels:
+        pixels[i, j] = [i / (width + 1), j / (height + 1), 0.25]
+
+gui = ti.GUI(name='Render', res=(width, height), show_gui=True)
+paint()
+gui.set_image(pixels)
+gui.show("output.png")
+
